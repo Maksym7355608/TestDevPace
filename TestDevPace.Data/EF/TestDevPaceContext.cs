f80e7b7eec88f97bb8fa5810317c4d57f862b2b8
Maksym <mmajor143@gmail.com>
2022-09-03 21:54:12 +0300

    Add etity and db context

---
+++ TestDevPace.Data/EF/TestDevPaceContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestDevPace.Data.Entities;
+
+namespace TestDevPace.Data.EF
+{
+    public class TestDevPaceContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public TestDevPaceContext(DbContextOptions<TestDevPaceContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public TestDevPaceContext()
+        { }
+    }
+}
---
+++ TestDevPace.Data/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace TestDevPace.Data.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
